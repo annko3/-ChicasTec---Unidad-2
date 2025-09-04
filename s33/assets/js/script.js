6438575e00076c342769c8cb4ac226b4fad6d78f
Jia <r.s.nicole1102@gmail.com>
2025-09-04 18:40:11 -0500

    s33 actualizada con comentarios

--- s33/assets/js/script.js
+++ s33/assets/js/script.js
@@ -1,180 +1,140 @@
 /*
-POO en JavaScript
+Seleccionamos elementos del DOM gracias al objeto "document" y algunos de sus metodos:
 
-Clase -> Es el molde de mi objeto y determina de una manera generica sus atributos y metodos.
-
-  - Atributos: "Caracteristicas" del objeto, son representados por variables.
-  - Metodos: Son "funciones o comportamientos" del objeto, son representados por funciones.
-
-En ECMAScript 6, se introdujo la palabra reservada "class" para la sintaxis de objetos, antes de eso se usaba "function", de todas maneras esto sigue funcionando.
-
-JavaScript NO IMPLEMENTA DIRECTAMENTE LA POO, simplemente es una especie de "mascara" que la simula.
-
-Explicacion sintaxis:
-
-1. Palabra reservada "class" indica que vamos a crear una clase.
-2. El nombre de la clase, debe ir en PascalCase. Es muy parecido a camelCase, solo que la primera letra tambien es mayuscula.
-3. Abrimos llaves para indicar el bloque de codigo de la clase.
-4. Metodo constructor. Es un metodo especial que nos indica los atributos de la clase.
-5. Pasar los atributos de nuestra clase como parametros del metodo constructor.
-6. Dentro del metodo constructor, INICIALIZAMOS NUESTROS ATRIBUTOS. Esto significa decirle a la clase que ESE atributo va a tomar cierto valor. this -> significa "esto" y es una palabra reservada que hace referencia al propio objeto.
-7. Luego agregamos los metodos, ya dijimos que son funciones, pero los metodos no llevan la palabra reservada "function".
-
-Sintaxis
-
-class NombreClase {
-  constructor(atributo1, atributo2) {
-    this.atributo1 = atributo1;
-    this.atributo2 = atributo2;
-  }
-
-  metodo() {
-    // codigo del metodo...
-  }
-}
+getElementById("nombre-id") -> Obtiene el elemento con el id "nombre-id".
 */
+// Seleccionando un elemento por ID y almacenandolo en una variable
+let mainTitle = document.getElementById("main-title");
 
-// Ejemplo
-// Creamos una clase de persona con los atributos y metodos vistos en el diagrama
+// Mostramos el elemento
+console.log(mainTitle);
+// Mostramos el contenido de mainTitle
+// Si no se ha encontrado un elemento, esto va a dar error
+console.log(mainTitle.innerText);
+// Actualizamos el contenido dinamicamente
+mainTitle.innerText = "OTRO TITULO";
+// Mostramos el contenido de mainTitle nuevamente
+console.log(mainTitle.innerText);
 
-// Palabra reservada class y nombre en PascalCase
-class Person {
-  // Encapsulamiento, exponer solo los atributos que necesito
-  // #nombreAtributo, van antes del metodo constructor
-  #cellphone;
+// getElementsByClassName("nombre-clase") -> Obtiene todos los elementos con la clase "nombre-clase".
+// Esto devuelve un arreglo, aunque solo sea un elemento
+let mainTexts = document.getElementsByClassName("main-text");
+console.log(mainTexts); // Esto es un ARREGLO y sus elementos son Elementos HTML
+// Accedemos a un elemento y mostramos su contenido
+console.log(mainTexts[0].innerText);
+// Modificamos el contenido
+mainTexts[0].innerText = "PARRAFO CAMBIADO";
 
-  // Metodo constructor y pasamos los atributos como parametros
-  constructor(firstName, lastName, age, height, weight, hobbies, cellphone) {
-    // Inicializamos los atributos
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.height = height;
-    this.weight = weight;
-    this.hobbies = hobbies;
-    this.#cellphone = cellphone;
-  }
+// getElementsByTagName("nombre") -> Obtiene todos los elementos con la etiqueta "nombre".
+let subtitles = document.getElementsByTagName("h2");
+console.log(subtitles);
+console.log(subtitles[0].innerText);
+console.log(subtitles[1].innerText);
+console.log(subtitles[2].innerText);
 
-  // Agregamos los metodos necesarios
-  sayHello() {
-    // Codigo de mi metodo
-    console.log("Hola desde el metodo!");
-  }
-
-  // Usando los atributos
-  introduce() {
-    console.log(
-      `Hola buenas noches, me llamo ${this.firstName} ${this.lastName} y tengo ${this.age} años.`
-    );
-  }
-
-  // Mostrando un atributo protegido
-  showCellphone() {
-    return this.#cellphone;
-  }
-}
-
-// Instanciar un objeto, usamos la palabra reservada new
-// let nombreObjeto = new NombreClase(atributo1, atributo2, ...);
-// Almacenamos ese objeto en una variable.
-let person1 = new Person(
-  "Valeria",
-  "Lozano",
-  24,
-  1.61,
-  57,
-  ["programar", "armar el cubo de rubik"],
-  "987654321"
-);
-
-// Mostramos en consola
-console.log(person1);
-// Accediendo a los atributos
-console.log(person1.firstName);
-console.log(person1.hobbies);
-
-console.log("METODOS");
-// Llamando a mi metodo
-person1.sayHello();
-person1.introduce();
-
-// Intentamos acceder al atributo oculto por encapsulamiento
-// console.log(person1.#cellphone);  // error
-// console.log(person1.cellphone);  // undefined
-
-console.log("ENCAPSULAMIENTO");
-// Accediendo al atributo desde un metodo
-console.log(person1.showCellphone());
-
+console.log("SELECTOR POR CSS");
 /*
-HERENCIA
+Selectores por CSS, se llaman asi porque usan la sintaxis de un selector CSS para seleccionar los elementos del DOM.
 
-Es la capacidad de una clase de poder heredar los atributos y metodos de la otra. Clase Padre -> Clase Hija
+querySelector("selector") -> Devuelve el primer elemento que coincida con el selector.
+querySelectorAll("selector") -> Devuelve un arreglo de elementos que coincidan con el selector.
 */
-console.log("HERENCIA");
+// let mainTitle = document.getElementById("main-title");
+let queryMainTitle = document.querySelector("#main-title");
+console.log(queryMainTitle);
 
-// Vamos a crear una clase hija a partir de la clase Person
-// Ejemplo
-// Crea una clase de Chef a partir de Person
-class Chef extends Person {
-  // Si yo quiero agregar mas atributos, debo escribir el constructor de la siguiente manera
-  // Debe contener TODOS los atributos de la clase padre
-  constructor(
-    firstName,
-    lastName,
-    age,
-    height,
-    weight,
-    hobbies,
-    cellphone,
-    restaurant,
-    favoriteFood
-  ) {
-    // Primero pasas los atributos a la clase padre -> super()
-    super(firstName, lastName, age, height, weight, hobbies, cellphone);
-    // Luego de super(), agregas tus atributos nuevos. ESTOS NUEVOS ATRIBUTOS NO SE PASAN A LA CLASE PADRE
-    this.restaurant = restaurant;
-    this.favoriteFood = favoriteFood;
-  }
+// let mainTexts = document.getElementsByClassName("main-text");
+let queryMainTexts = document.querySelectorAll(".main-text");
+console.log(queryMainTexts);
 
-  // Podemos sobreescribir metodos del padre, probemos sobreescribiendo introduce()
-  introduce() {
-    console.log(
-      `Hola me llamo ${this.firstName} ${this.lastName} y soy chef en el restaurante ${this.restaurant}.`
-    );
-  }
+// EVENTOS
+console.log("EVENTOS");
 
-  cook() {
-    console.log(
-      `El chef ${this.lastName} esta cocinando ${this.favoriteFood}...`
-    );
-  }
-}
+/*Selecciono un elemento y le agrego un evento. No es necesario almacenar el elemento en alguna variable
 
-// Instanciamos un objeto de Chef
-let chef1 = new Chef(
-  "Alfredo",
-  "Linguini",
-  24,
-  1.61,
-  65,
-  ["Cocinar", "Lavar platos"],
-  "987654321",
-  "Guestau",
-  "Lasagna"
-);
+[SELECCIONAR ELEMENTO].addEventListener("nombre-evento", function () {
+  // FUNCION A EJECUTAR CUANDO SUCEDA ESE EVENTO
+})
 
-console.log(chef1.firstName);
-console.log(chef1.favoriteFood);
-chef1.sayHello();
-chef1.cook();
-
-// LOS NUEVOS ATRIBUTOS Y METODOS DE CHEF NO FUNCIONAN EN PERSON
-
-/*
-POLIMORFISMO
-
-Permite que un metodo se comporte de manera distinta de acuerdo al objeto que lo llame.
+La funcion es una funcion anonima, no tiene nombre.
 */
-chef1.introduce();
-person1.introduce();
+document.getElementById("main-button").addEventListener("click", function () {
+  // CODIGO A EJECUTAR CUANDO SUCEDA EL EVENTO
+  console.log("BOTON PRESIONADO");
+
+  let mainButton = document.getElementById("main-button");
+
+  // Podemos cambiar estilos
+  let currentBackgroundColor = document.body.style.backgroundColor;
+
+  // Cambiado color de forma condicional
+  if (currentBackgroundColor == "red") {
+    document.body.style.backgroundColor = "white";
+    mainButton.style.color = "white";
+  } else {
+    document.body.style.backgroundColor = "red";
+    mainButton.style.color = "red";
+  }
+
+  // Obtener el valor ingresado en un input
+  // Primero seleccionamos el input
+  // Accedemos a la propiedad value
+  let magicNumber = document.querySelector("#magic-number").value;
+
+  console.log(`Valor ingresado en el input: ${magicNumber}`);
+
+  // Mostramos el valor en el contenido de un elemento
+  let greetParagraph = document.querySelector("#greet");
+
+  // Validacion en caso el input esté vacío
+  if (magicNumber == "" || magicNumber.length === 0) {
+    greetParagraph.innerText = "NO INGRESASTE UN NOMBRE";
+  } else {
+    greetParagraph.innerText = `Buenas noches ${magicNumber}`;
+  }
+});
+
+// Evento de click en otro boton
+// Selecionamos el boton
+let togglerButton = document.querySelector("#toggler");
+// Fijamos el contenido
+togglerButton.innerText = "MOSTRAR";
+// Seleccionamos el texto
+let hiddenText = document.querySelector("#hidden-text");
+// Lo ocultamos directamente
+hiddenText.style.display = "none";
+
+// Agregamos el event listener
+togglerButton.addEventListener("click", function () {
+  let currentTextDisplay = hiddenText.style.display;
+
+  // Si el boton esta oculto
+  if (currentTextDisplay == "none") {
+    // Lo muestro
+    hiddenText.style.display = "block";
+    togglerButton.innerText = "OCULTAR";
+  } else {
+    // Si no esta oculto, lo oculto
+    hiddenText.style.display = "none";
+    togglerButton.innerText = "MOSTRAR";
+  }
+});
+
+// FORMA CORRECTA DE CAMBIAR ESTILOS, USANDO CLASES
+let togglerButton2 = document.querySelector("#toggler-2");
+// Fijamos el contenido
+togglerButton2.innerText = "MOSTRAR";
+// Seleccionamos el texto
+let hiddenText2 = document.querySelector("#hidden-text-2");
+
+// Agregamos el event listener
+togglerButton2.addEventListener("click", function () {
+  if (hiddenText2.classList.contains("hide-text")) {
+    hiddenText2.classList.remove("hide-text");
+    togglerButton2.innerText = "OCULTAR";
+  } else {
+    console.log("ELSE");
+    hiddenText2.classList.add("hide-text");
+    togglerButton2.innerText = "MOSTRAR";
+  }
+});
